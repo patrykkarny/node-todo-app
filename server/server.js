45d8fb4348e1714ae2e37e490a6ac752a2a91d96
Patryk Karny <patryk.karny@infullmobile.com>
2018-03-18 20:33:57 +0100

    Added GET todo by id

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import { ObjectID } from 'mongodb';
 
-const { mongoose } = require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+import bodyParser from 'body-parser';
+
+import { mongoose } from './db/mongoose'; // eslint-disable-line no-unused-vars
+import { Todo } from './models/todo';
+import { User } from './models/user'; // eslint-disable-line
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Todo app</h1>');
+});
 
 app.post('/todos', (req, res) => {
   const todo = new Todo({
@@ -17,19 +23,28 @@ app.post('/todos', (req, res) => {
   todo.save()
     .then(doc => res.send(doc))
     .catch(err => res.status(400).send(err));
-
 });
 
 app.get('/todos', (req, res) => {
   Todo.find()
     .then(todos => res.send({ todos }))
     .catch(err => res.status(400).send(err));
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Invalid ID');
+  }
+
+  Todo.findById(id)
+    .then(todo => (todo ? res.send({ todo }) : res.status(404).send('Todo not found')))
+    .catch(() => res.status(400).send());
+});
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(3000, () => console.log('Started on port 3000'));
 }
 
-module.exports = {
-  app,
-};
+export { app };
