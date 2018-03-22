b5f8db1f16f265384b320343a3f0dd21a57720d2
Patryk Karny <patryk.karny@infullmobile.com>
2018-03-22 20:44:47 +0100

    Add seed for tests

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,48 @@
+import { ObjectID } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import { Todo } from '../../models/todo';
+import { User } from '../../models/user';
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+export const users = [{
+  _id: userOneId,
+  email: 'patryk@example.com',
+  password: 'samplePass!',
+  tokens: [{
+    access: 'auth',
+    token: sign({ _id: userOneId, access: 'auth' }, 'abc123').toString(),
+  }],
+}, {
+  _id: userTwoId,
+  email: 'kamil@example.com',
+  password: 'SimplePass11!',
+}];
+
+export const todos = [{
+  _id: new ObjectID(),
+  text: 'Test 1',
+}, {
+  _id: new ObjectID(),
+  text: 'Test 2',
+  completed: true,
+  completedAt: 123,
+}];
+
+export const populateTodos = (done) => {
+  Todo.remove({})
+    .then(() => Todo.insertMany(todos))
+    .then(() => done());
+};
+
+export const populateUsers = (done) => {
+  User.remove({})
+    .then(() => {
+      const userOne = new User(users[0]).save();
+      const userTwo = new User(users[1]).save();
+
+      return Promise.all([userOne, userTwo]);
+    })
+    .then(() => done());
+};
